64ed3176e2c230fe91d60d352a8a5e782b097cdb
Prasheekmankar <89675381+Prasheekmankar@users.noreply.github.com>
2021-10-29 05:27:01 -0400

    Add files via upload

---
+++ 25.js
@@ -0,0 +1,40 @@
+// console.log("hellow  ");
+
+// function urlBuilder(id,width,height){
+//     return "https://media.springernature.com/full/springer-cms/rest/v1/img/18893370/v1/height/320"
+// }
+
+
+// function fun({url,name,age=20}) {
+//     return `${url}?name=${name}&age=${age}`
+// }
+// console.log(fun({url:"http://www.google.com",x:"ram",eag:50}))
+
+
+// function double(n) {
+//     return [n, n*2];
+// }
+// const[t1,t2,t3=10]=double(6);
+// console.log(t1,t2,t3);
+
+// const stu = {name:"Sandeep",age : 12,gender:"male"};
+
+// console.log{...stu, age: 15}
+
+// const z1 = [1,4,5];
+// const z2 = z1;
+// const z3 = [1,4,5];
+// console.log(z1===z2)
+// console.log(z1===z3)
+
+
+// const result = [{name:"abc",age:16},
+// {name:"def",age:10},
+// {name:"ghi",age:30},
+// {name:"good",age:18}
+// ]
+// .map(({age})=>age*2)
+// .filter((n)=>n>25);
+// console.log(result);
+
+ 
